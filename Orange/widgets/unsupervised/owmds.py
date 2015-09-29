f2f34ef34480ff15a2bbb29cfc317e8d6c891056
janezd <janez.demsar@fri.uni-lj.si>
2015-09-29 19:42:14 +0200

    OWMDS: Change settings to caption

--- Orange/widgets/unsupervised/owmds.py
+++ Orange/widgets/unsupervised/owmds.py
@@ -24,6 +24,7 @@ import Orange.projection
 import Orange.distance
 import Orange.misc
 from Orange.widgets.io import FileFormats
+from Orange.canvas import report
 
 
 def torgerson(distances, n_components=2):
@@ -993,14 +994,16 @@ class OWMDS(widget.OWWidget):
     def send_report(self):
         if self.data is None:
             return
-        self.report_items((
+        self.report_plot(self.plot)
+        caption = report.render_items_vert((
             ("Color", self.color_value != "Same color" and self.color_value),
             ("Shape", self.shape_value != "Same shape" and self.shape_value),
-            ("Size",  self.size_value != "Same size" and self.size_value),
-            ("Labels", self.label_value != "No labels" and self.label_value),
-            ("Output", self.output_combo.currentText())
-        ))
-        self.report_plot(self.plot)
+            ("Size", self.size_value != "Same size" and self.size_value),
+            ("Labels", self.label_value != "No labels" and self.label_value)))
+        if caption:
+            self.report_caption(caption)
+        self.report_items((("Output", self.output_combo.currentText()),))
+
 
 
 def colors(data, variable, palette=None):
